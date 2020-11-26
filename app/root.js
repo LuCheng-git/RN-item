8cba0f80b05e47d83b2b2f9ff2ce1d5c686bf642
LuCheng-git <1456096100@qq.com>
2020-11-26 14:40:36 +0800

    配置Hello World

---
+++ app/root.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text,View} from 'react-native';
+
+const App:() => React$Node = () => {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  )
+}
+export default App;
